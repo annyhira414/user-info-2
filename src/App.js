a7289b71dcdb706c8d6177510c980a9d04410055
annyhira414 <annyhira@gmail.com>
2022-03-08 14:53:16 +0600

    validation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,103 @@
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Table from "../src/components/Table";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [editingIndex, setEditingIndex] = useState(null);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+
+
+    const doSave = () => {
+      if (name && email && address) {
+      const user = {
+        name,
+        email,
+        address,
+      };
+      setData([...data, user]);
+      setName("");
+      setEmail("");
+      setAddress("");
+    }
+    else{
+      alert("hlw")
+    }
+  } ;
+
+  
+
+  const deleteRow = (index) => {
+    let newData = [...data];
+    newData.splice(index, 1);
+    setData(newData);
+  };
+
+  const editRow = (index) => {
+    const row = data[index];
+    setEditingIndex(index);
+    setName(row.name);
+    setEmail(row.email);
+    setAddress(row.address);
+  };
+
+  const doUpdate = () => {
+    let newData = [...data];
+    let row = newData[editingIndex];
+    row.name = name;
+    row.email = email;
+    row.address = address;
+    newData[editingIndex] = row;
+    setData(newData);
+    setName("");
+    setEmail("");
+    setAddress("");
+    setEditingIndex(null);
+  };
+
   return (
     <div className="App">
-      <h1>hlw i am app.js</h1>
+      <div style={{ display: "inline-block" }}>
+        <div>
+          <label>Name</label>
+          <br />
+          <input
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+          <br />
+          <label>Email</label>
+          <br />
+          <input
+            type="text"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+          <br />
+          <label>Address</label>
+          <br />
+          <input
+            type="text"
+            onChange={(e) => setAddress(e.target.value)}
+            value={address}
+          />
+          <br />
+          {editingIndex !== null ? (
+            <>
+              <button onClick={doUpdate}>Update</button>
+              <button onClick={() => setEditingIndex(null)}>Add new</button>
+            </>
+          ) : (
+            <button onClick={doSave}>Save</button>
+          )}
+        </div>
+      </div>
+      <div style={{ width: "50%", display: "inline-block" }}>
+        <Table tableData={data} deleteRow={deleteRow} editRow={editRow} />
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Table from './components/Table';
 
 ReactDOM.render(
   <React.StrictMode>
